1361d9d6032147c27e4996aa3e2e075107bb13de
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:25:29 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/Actions/ConfigurationActionPriority.cs
@@ -0,0 +1,10 @@
+﻿namespace microservice_1.Actions
+{
+    /// <summary>
+    /// Delete if not needed
+    /// </summary>
+    public class ConfigurationActionPriority
+    {
+        public static readonly int SomePriority = 44;
+    }
+}
