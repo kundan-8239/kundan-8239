66c8ba9795c660df1fd2fec9b7e199c1843c3c12
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:25:47 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("microservice_1")]
+[assembly: AssemblyDescription("microservice_1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WellSky")]
+[assembly: AssemblyProduct("New Microservice in existing repo")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("820CB74F-EB3C-45E7-84A3-72445B06E774")]
+
+[assembly: InternalsVisibleTo("microservice_1.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
