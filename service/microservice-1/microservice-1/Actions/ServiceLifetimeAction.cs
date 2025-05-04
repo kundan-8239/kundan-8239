35c0e2cb79f84b2fccce7d49aa06f4c3179c1004
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:25:31 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/Actions/ServiceLifetimeAction.cs
@@ -0,0 +1,30 @@
+﻿using Atlas.Actions;
+using Atlas.Utilities;
+using JetBrains.Annotations;
+
+namespace microservice_1.Actions
+{
+    /// <summary>
+    /// Delete if not needed
+    /// </summary>
+    internal class ServiceLifetimeAction : IServiceLifetimeAction
+    {
+        public int Priority { get; } = LifetimeActionPriority.SomePriority;
+
+        public ServiceLifetimeAction()
+        {
+        }
+
+        public void OnStart()
+        {
+        }
+
+        public void OnChange()
+        {
+        }
+
+        public void OnStop()
+        {
+        }
+    }
+}
