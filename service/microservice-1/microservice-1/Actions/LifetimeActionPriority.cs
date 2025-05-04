2560c052cf1fa28c34ff307a8aa128894b8f22a6
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:53:03 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/Actions/LifetimeActionPriority.cs
@@ -0,0 +1,10 @@
+﻿namespace microservice_1.Actions
+{
+    /// <summary>
+    /// Delete if not needed
+    /// </summary>
+    public class LifetimeActionPriority
+    {
+        public static readonly int SomePriority = 444;
+    }
+}
