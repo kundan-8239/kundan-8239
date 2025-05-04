98891e9d963b887033a4965eaa143989de657b33
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:53:15 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/Models/UpdateThingRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace microservice_1.Models
+{
+    /// <summary>
+    /// Request for creating a thing.
+    /// </summary>
+    /// 
+    public class UpdateThingRequest
+    {
+        /// <summary>
+        /// The new name for the thing
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name cannot be empty")]
+        public string NewName { get; set; }
+    }
+}
