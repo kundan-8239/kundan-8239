1b1a3018200fe83eed2523560347be179e4312f9
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:53:11 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/microservice-1.cs
@@ -0,0 +1,12 @@
+﻿using Atlas.Authentication;
+using Atlas.WebService;
+
+namespace microservice_1
+{
+    /// <inheritdoc />
+    public class microservice-1 : AtlasService
+    {
+	    /// <inheritdoc />
+        public override string ServiceName { get; } = "microservice-1";
+    }
+}
