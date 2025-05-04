ac08062fcf36695664a65fd294f88a62a0d9181d
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-04 23:25:45 +0530

    New Microservice in existing repo

---
+++ service/microservice-1/microservice-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Atlas.WebService;
+
+namespace microservice_1
+{
+    /// <summary>
+    /// Entry point class
+    /// </summary>
+    public class Program
+    {
+        /// Entry point method
+        public static void Main(string[] args)
+        {
+            new microservice-1().Run(args);
+        }
+    }
+}
