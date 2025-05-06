c028e66dcc89855252b087a1a74a4be75aa4aad5
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-06 11:27:17 +0530

    New Microservice in existing repo

---
+++ temp-service/microservice-1/microservice-1/Auditing/ThingRenamedAuditTrail.cs
@@ -0,0 +1,12 @@
+﻿namespace microservice_1.Auditing
+{
+    internal class ThingRenamedAuditTrail : ThingAuditTrail
+    {
+        public string PreviousName { get; set; }
+
+        public ThingRenamedAuditTrail(long? thingId, string newName, string previousName, bool succeeded):base(AuditExtensions.ActionUpdated, thingId, newName, succeeded)
+        {
+            PreviousName = previousName;
+        }
+    }
+}
