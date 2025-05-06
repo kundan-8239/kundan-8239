f7904064a6be6c75036c95dfadbf974082c02b56
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-06 11:27:17 +0530

    New Microservice in existing repo

---
+++ temp-service/microservice-1/microservice-1/Auditing/ThingAuditTrail.cs
@@ -0,0 +1,23 @@
+﻿using Atlas.Auditing;
+
+namespace microservice_1.Auditing
+{
+    internal class ThingAuditTrail : AuditTrailLog
+    {
+        public string Action { get; }
+
+        public long? ThingId { get; }
+
+        public string ThingName { get; }
+
+        public bool Succeeded { get; }
+
+        public ThingAuditTrail(string action, long? thingId, string thingName, bool succeeded)
+        {
+            Action = action;
+            ThingId = thingId;
+            ThingName = thingName;
+            Succeeded = succeeded;
+        }
+    }
+}
