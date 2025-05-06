ebf9914707eb5a91bf5f8f3d4e92bcea3d30b650
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-06 11:27:12 +0530

    New Microservice in existing repo

---
+++ temp-service/microservice-1/microservice-1/Actions/ServicesConfigurationAction.cs
@@ -0,0 +1,38 @@
+﻿using Atlas.Actions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace microservice_1.Actions
+{
+    /// <summary>
+    /// Action for configuring dependency injection
+    /// </summary>
+    /// <remarks>
+    /// For IConfigureServicesAction:
+    /// * ConfigureServices() performs DI registrations
+    /// </remarks>
+    internal class ServicesConfigurationAction : IConfigureServicesAction
+    {
+        //private readonly SomeOptions _someOptions;
+
+        /// <inheritdoc />
+        public ServicesConfigurationAction
+        (
+        //IOptions<SomeOptions> someOptions
+        )
+        {
+            // configuration settings from ConfigurationKey attributes
+            //_someOptions = someOptions.Value;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Perform all DI registrations.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            //services.AddSingleton<ISomething, Something>();
+        }
+    }
+}
