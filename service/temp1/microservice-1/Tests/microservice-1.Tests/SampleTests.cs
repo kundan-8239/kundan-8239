e7fb927c923c848de7a7e67e2109eb1242ac1eb8
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-07 15:33:14 +0530

    New Microservice in existing repo

---
+++ service/temp1/microservice-1/Tests/microservice-1.Tests/SampleTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace microservice_1.Tests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [SetUp]
+        public void SetUpTests()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void AlwaysTrue()
+        {
+            Assert.True(true);
+
+            var sample = true;
+            sample.Should().BeTrue();
+        }
+    }
+}
