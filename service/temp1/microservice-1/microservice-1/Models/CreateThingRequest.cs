d6c098759d6b3d850c202024793044e61442e247
Kundan Kumar <kundan.kumar@wellsky.com>
2025-05-07 15:33:04 +0530

    New Microservice in existing repo

---
+++ service/temp1/microservice-1/microservice-1/Models/CreateThingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace microservice_1.Models
+{
+    /// <summary>
+    /// Request for creating a thing.
+    /// </summary>
+    public class CreateThingRequest
+    {
+        /// <summary>
+        /// The name of the thing
+        /// </summary>
+       [Required]
+       public string Name { get; set; }
+    }
+}
